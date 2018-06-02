7f91c61194f693415476516ab846b2b126d145a5
galakerman <akermang@gmail.com>
2018-06-02 13:35:23 +0300

    clear input value if not in list

--- js/script.js
+++ js/script.js
@@ -1,38 +1,51 @@
-const data = ["gal","lior","nir","asaf","ben","rammy","galgal","ban","banban"];
-// keyFilter --> string 
+const data = ["gal", "lior", "nir", "asaf", "ben", "rammy", "galgal", "ban", "banban"];
 
-function filterList(listArr,val){
-  let newList = listArr.filter(function(item){
-    return  item.startsWith(val);
+const setInputVal = function (value) {
+  input.val(value);
+  let emptyList = []
+  renderList(emptyList);
+}
+
+function renderList(list) {
+  let element = $("#ul-a-container");
+  let ul = document.createElement("ul");
+
+  list.map(function (item) {
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span.innerText = item;
+    li.appendChild(span);
+    span.addEventListener("click", setInputVal.bind(event, item))
+    ul.appendChild(li);
+  })
+  element.html(ul);
+}
+renderList(data);
+
+function filterList(listArr, val) {
+  let newList = listArr.filter(function (item) {
+    return item.startsWith(val);
   });
-  renderList(newList)
+  if (newList.length > 0) {
+    renderList(newList)
+  }
+  else {
+    resetInput();
+  }
   return newList;
 }
 
 let input = $("#ap-autocomplit-input");
-input.keyup(function() {
+input.keyup(function () {
   let val = input.val();
   filterList(data, val);
 });
+input.focus()
 
-const setInputVal = function (value) {
-    input.val(value); 
-    let newList = []
-    renderList(newList);
+function resetInput() {
+  input.val("");
+  renderList(data);
 }
 
-function renderList(list){
-    let element = $("#ul-a-container");
-    let ul = document.createElement("ul");
-   
-    list.map(function(item){
-        let li = document.createElement("li");
-        let span = document.createElement("span");
-        span.innerText = item;
-        li.appendChild(span);
-        span.addEventListener("click",setInputVal.bind(event,item) )
-        ul.appendChild(li);
-    })
-    element.html(ul);
-}
-renderList(data);
+
+
